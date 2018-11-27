aaf886decb31508b1efc98288222f0527e6b3fb3
a1709 <ygbox0324@gmail.com>
2018-11-27 12:18:43 +0900

    Add source to display URL.

--- scripts/allTabExport.js
+++ scripts/allTabExport.js
@@ -1,4 +1,7 @@
-chrome.tabs.getSelected(null, tab => {
+chrome.tabs.query({ windowType: "normal" }, function(tabs) {
   let title = document.getElementById("title");
-  title.innerHTML = tab.title;
+  for (var i = tabs.length - 1; i >= 0; i--) {
+    title.insertAdjacentHTML("afterbegin", `${i + 1}. ${tabs[i].title}<br>`);
+  }
+  chrome.browserAction.setBadgeText({ text: String(tabs.length) });
 });
