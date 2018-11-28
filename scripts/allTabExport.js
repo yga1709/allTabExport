8ab8a6bd1ac1a52396a46a0db75dd13de668beaa
a1709 <ygbox0324@gmail.com>
2018-11-28 10:24:38 +0900

    add json perser and download function.

--- scripts/allTabExport.js
+++ scripts/allTabExport.js
@@ -1,18 +1,76 @@
-var exportUrls = {};
+document.getElementById("export").onclick = () => {
+  let exportUrls = {};
+  exportUrls[`num`] = 0;
+  chrome.tabs.query({ windowType: "normal" }, tabs => {
+    let title = document.getElementById("title");
+    let dispStr;
+    let checkUrl = document.createElement("a");
 
-chrome.tabs.query({ windowType: "normal" }, function(tabs) {
-  let title = document.getElementById("title");
-  for (var i = tabs.length - 1; i >= 0; i--) {
-    //popupに 「1. タイトル」の形式で表示。
-    title.insertAdjacentHTML("afterbegin", `${i + 1}. ${tabs[i].title}<br>`);
-    //Export用にexportUrls（連想配列）の中にタイトル（key:titles〇)とurl(key:urls〇)を追加。
-    exportUrls[`titles${String(i)}`] = tabs[i].title;
-    exportUrls[`urls${String(i)}`] = tabs[i].url;
-    console.log(
-      exportUrls[`titles${String(i)}`],
-      exportUrls[`urls${String(i)}`]
+    for (var i = tabs.length - 1; i >= 0; i--) {
+      checkUrl.href = tabs[i].url;
+
+      dispStr = `${i + 1}. ${tabs[i].title}`;
+
+      //popupに 「1. タイトル」の形式で表示。
+      title.insertAdjacentHTML("afterbegin", `${dispStr}<br>`);
+
+      //Export用にexportUrls（連想配列）の中にタイトル（key:titles〇)とurl(key:urls〇)を追加。
+      exportUrls[`titles${String(i + 1)}`] = dispStr;
+      exportUrls[`urls${String(i + 1)}`] = tabs[i].url;
+      exportUrls[`num`] = tabs.length;
+      /*console.log(
+        exportUrls[`titles${String(i)}`],
+        exportUrls[`urls${String(i)}`]
+      );*/
+    }
+    //その時Exportする予定のタブの数をアイコン下に表示
+    chrome.browserAction.setBadgeText({ text: String(tabs.length) });
+
+    document.getElementById("export").disabled = true;
+    document.getElementById(
+      "save"
+    ).innerHTML = `<button id="saveButton">保存</button>`;
+    document.getElementById("saveButton").onclick = () => {
+      download();
+    };
+  });
+
+  download = data => {
+    let json = JSON.stringify(exportUrls);
+    let blob = new Blob([json], { type: "application/json" });
+    let fileStamp = getTime();
+    let a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    console.log(a.href);
+    a.target = "_blank";
+    chrome.downloads.download(
+      {
+        url: a.href,
+        filename: `sample/AllTabExport${fileStamp}.json`
+      },
+      e => console.log(e)
     );
-  }
-  //その時Exportする予定のタブの数をアイコン下に表示
-  chrome.browserAction.setBadgeText({ text: String(tabs.length) });
-});
+  };
+
+  /*document.getElementById("saveButton").addEventListener("click", () => {
+    download(exportUrls);
+  });*/
+};
+
+getTime = () => {
+  let jikan = new Date();
+  //時・分・秒を取得する
+  let hour = jikan.getHours();
+  let minute = jikan.getMinutes();
+  let second = jikan.getSeconds();
+
+  let hiduke = new Date();
+  //年・月・日・曜日を取得する
+  let year = hiduke.getFullYear();
+  let month = hiduke.getMonth() + 1;
+  let week = hiduke.getDay();
+  let day = hiduke.getDate();
+
+  let fileStamp = `${year}-${month}-${week}-${day}-${hour}-${minute}-${second}`;
+  return fileStamp;
+};
